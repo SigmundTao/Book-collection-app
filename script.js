ce5b47012137a6a181f278d1a5083e416850cdcc
Tao Simon <sigmundTao@gmail.com>
2025-11-11 22:00:37 +0000

    Scrapped project, api working

--- script.js
+++ script.js
@@ -1,99 +1,115 @@
+const API_KEY = 'AIzaSyABeY9VBDmPih7W8nOf5zndu9I5MtF0wfQ';
+
 const container = document.getElementById('container');
+const searchBtn = document.getElementById('search-btn');
+const searchBar = document.getElementById('search-bar');
 
-function generateStartScreen(){
-    const title = document.createElement('h2');
-    title.classList.add('start-screen-title');
-    title.innerText = "Book collection App";
-
-    const createNewLibraryBtn = document.createElement('button');
-    createNewLibraryBtn.classList.add('new-library-btn');
-    createNewLibraryBtn.innerText = 'Create New Library';
-
-    createNewLibraryBtn.addEventListener('click', () => {
-        createProfile()
-    })
-
-    const uploadLibraryBtn = document.createElement('button');
-    uploadLibraryBtn.classList.add('upload-library-btn');
-    uploadLibraryBtn.innerText = 'Upload Existing Library';
-
-    const btnHolder = document.createElement('div');
-    btnHolder.classList.add('start-screen-btn-holder');
-    btnHolder.style.border = '1px solid black';
-
-    btnHolder.appendChild(createNewLibraryBtn);
-    btnHolder.appendChild(uploadLibraryBtn);
-
-    container.appendChild(title);
-    container.appendChild(btnHolder);
-}
-
-function createProfile(){
-    container.innerHTML = '';
-
-    const profileCard = document.createElement('div');
-    profileCard.classList.add('profile-card');
-
-    const profilePicture = document.createElement('div');
-    profilePicture.classList.add('profile-picture');
-    profilePicture.style.backgroundSize = 'contain'
-    profilePicture.style.backgroundPosition = 'center';
-
-    const profilePicturesHolder = document.createElement('div');
-    profilePicturesHolder.classList.add('profile-pictures-holder');
-    
-    defaultProfilePictures.forEach(picture => {
-        const pictureDiv = document.createElement('div');
-        pictureDiv.classList.add('pfp');
-
-        pictureDiv.style.backgroundImage = `url('${picture.url}')`;
-        pictureDiv.style.backgroundPosition = 'center';
-        pictureDiv.style.backgroundRepeat = 'no-repeat';
-        pictureDiv.style.backgroundSize = 'contain';
-
-        profilePicturesHolder.appendChild(pictureDiv);
-
-        pictureDiv.addEventListener('click', () => {
-            const pics = document.querySelectorAll('.pfp')
-            pics.forEach(pic => {
-                pic.classList.remove('selected-pic');
-            })
-            pictureDiv.classList.add('selected-pic');
-
-            const selectedPic = document.querySelector('.selected-pic');
-            profilePicture.style.backgroundImage = selectedPic.style.backgroundImage;
-        })
-    })
-
-
-    const name = document.createElement('input');
-    name.type = 'text';
-
-    profileCard.appendChild(profilePicture);
-    profileCard.appendChild(profilePicturesHolder);
-    profileCard.appendChild(name);
-
-    container.appendChild(profileCard);
-}
-
-function saveProfile(profilePicture, name){
-    user.name = name;
-    user.profilePicture = `url('${profilePicture}')`;
-}
-
-const defaultProfilePictures = [
-    {name: 'thorfinn', url: 'https://i.pinimg.com/736x/66/a3/d2/66a3d23902ea634936cbd3c3c523f1c1.jpg'},
-    {name: 'musashi', url: 'https://i.pinimg.com/736x/73/56/08/735608b60b4de86bbfbf7adedffdc854.jpg'},
-    {name: 'mugen', url: 'https://i.pinimg.com/736x/0a/af/34/0aaf34b5ee8e05345a13196ede815d4d.jpg'},
-    {name: 'jin', url: 'https://i.pinimg.com/1200x/d1/3e/41/d13e41da95b3416e669f4dbc7e8e0624.jpg'},
-    {name: 'spike', url: 'https://i.pinimg.com/736x/7e/4c/45/7e4c457ae4ba547cd5616aaa825953f6.jpg'},
-    {name: 'guts', url: 'https://i.pinimg.com/736x/07/33/3a/07333a56d4c0da5dc5afb5a365906872.jpg'},
-    {name: 'okdabe', url: 'https://i.pinimg.com/736x/5e/6b/ee/5e6beeb749ec884b9dc6ac4dd2657142.jpg'},
-];
+searchBtn.addEventListener('click', () => {
+    searchBook()
+})
 
 const user = {
-    name: null,
-    profilePicture: null
+    books: []
 }
 
-generateStartScreen()
+const bookHolder = document.createElement('div');
+
+function addBook(){
+
+}
+
+function deleteBook(){
+
+}
+
+async function searchBook(){
+    const bookTitle = searchBar.value;
+    const searchURL = `https://www.googleapis.com/books/v1/volumes?q=${bookTitle}&key=${API_KEY}`
+
+
+    const response = await fetch(searchURL);
+
+    const data = await response.json();
+    console.log(data);
+    displaySearchResults(data)
+}
+
+function displayView(){
+
+}
+
+function displaySearchResults(data){
+    container.innerHTML = '';
+
+    data.items.forEach(book => {
+        const bookTitle = book.volumeInfo.title || 'No title';
+        const authors = book.volumeInfo.authors || ['Unknown author'];
+        const description = book.volumeInfo.description || 'No description';
+        const identifiers = book.volumeInfo.industryIdentifiers || [];
+        const language = book.volumeInfo.language || 'N/A';
+        const categories = book.volumeInfo.categories || [];
+        const image = book.volumeInfo.imageLinks?.thumbnail || 'placeholder.jpg';
+
+        const result = document.createElement('div');
+        result.classList.add('search-result');
+
+        const resultImage = document.createElement('div');
+        resultImage.style.backgroundImage = `url(${image})`;
+        resultImage.classList.add('search-result-img');
+        
+        const title = document.createElement('h2');
+        title.innerText = bookTitle;
+
+        const authorsHolder = document.createElement('div');
+        authors.forEach(person => {
+           const contributer = document.createElement('div');
+           contributer.classList.add('search-result-author');
+           contributer.innerText = person;
+
+           authorsHolder.appendChild(contributer);
+        })
+
+        const descriptionHolder = document.createElement('p');
+        descriptionHolder.innerText = description;
+
+        const languageHolder = document.createElement('div');
+        languageHolder.classList.add('search-result-language-holder');
+        languageHolder.innerText = language;
+
+        const categoriesHolder = document.createElement('div');
+        categoriesHolder.classList.add('categories-holder');
+
+        categories.forEach(catergory => {
+            const c = document.createElement('div');
+            c.classList.add('catergory');
+            c.innerText = catergory;
+            categoriesHolder.appendChild(c);
+        })
+
+        const identifiersHolder = document.createElement('div');
+
+        identifiers.forEach(i => {
+            const type = document.createElement('div');
+            type.innerText = i.type;
+
+            const id = document.createElement('div');
+            id.innerText = i.identifier;
+
+            const div = document.createElement('div');
+            div.appendChild(type);
+            div.appendChild(id);
+
+            identifiersHolder.appendChild(div);
+        })
+
+        result.appendChild(resultImage);
+        result.appendChild(title);
+        result.appendChild(authorsHolder);
+        result.appendChild(descriptionHolder);
+        result.appendChild(languageHolder);
+        result.appendChild(categoriesHolder);
+        result.appendChild(identifiersHolder);
+
+        container.appendChild(result);
+    });
+}
